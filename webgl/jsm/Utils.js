36560561e7fc48e8c271d8eaeeb097008eea6fe1
dss <duanshuaishuai@pharmcube.com>
2023-10-11 00:21:51 +0800

    chore:poly

--- webgl/jsm/Utils.js
+++ webgl/jsm/Utils.js
@@ -28,4 +28,41 @@ function loadShader(gl, type, source) {
   return shader;
 }
 
-export { initShaders };
+function getMousePosInWebgl({ clientX, clientY }, canvas) {
+  //鼠标在画布中的css位置
+  const { left, top, width, height } = canvas.getBoundingClientRect();
+  const [cssX, cssY] = [clientX - left, clientY - top];
+  //解决坐标原点位置的差异
+  const [halfWidth, halfHeight] = [width / 2, height / 2];
+  const [xBaseCenter, yBaseCenter] = [cssX - halfWidth, cssY - halfHeight];
+  // 解决y 方向的差异
+  const yBaseCenterTop = -yBaseCenter;
+  //解决坐标基底的差异
+  return {
+    x: xBaseCenter / halfWidth,
+    y: yBaseCenterTop / halfHeight,
+  };
+}
+
+function glToCssPos({ x, y }, { width, height }) {
+  const [halfWidth, halfHeight] = [width / 2, height / 2];
+  return {
+    x: x * halfWidth,
+    y: -y * halfHeight,
+  };
+}
+
+//线性比例尺
+function ScaleLinear(ax, ay, bx, by) {
+  const delta = {
+    x: bx - ax,
+    y: by - ay,
+  };
+  const k = delta.y / delta.x;
+  const b = ay - ax * k;
+  return function (x) {
+    return k * x + b;
+  };
+}
+
+export { initShaders, getMousePosInWebgl, glToCssPos, ScaleLinear };
